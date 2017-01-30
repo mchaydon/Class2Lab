e64676348b2ecd2a5738e717106a53a00603a611
Mike <Mike@DESKTOP-NFTLEA8>
2017-01-29 20:48:21 -0600

    Lab 4 completed.

---
+++ src/java/model/WelcomeService.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author Mike
+ */
+public class WelcomeService {
+    private Calendar calendar = Calendar.getInstance();
+    
+    public final String getTimeOfDay(){
+        calendar.setTime(new Date());
+        if (calendar.get(Calendar.HOUR_OF_DAY) >= 7 && calendar.get(Calendar.HOUR_OF_DAY) <= 11)
+        {
+            return "morning";
+        }
+        else if (calendar.get(Calendar.HOUR_OF_DAY) >= 12 && calendar.get(Calendar.HOUR_OF_DAY) <= 19)
+        {
+            return "afternoon";
+        }
+        else
+        {
+            return "evening";
+        }
+    }
+    
+    public final String getGreeting(String name)
+    {
+        return "Good " + getTimeOfDay() + ", " + name + ". Welcome!";
+    }
+}
---
+++ web/results.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : results
+    Created on : Jan 29, 2017, 8:20:59 PM
+    Author     : Mike
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>${greeting}</h1>
+        
+    </body>
+</html>
